5141d7394e04b448f5a3b1bfee7de5f4f41fe98f
Ilias Karimalis <iliaskarimalis@outlook.com>
2021-12-02 00:29:52 -0800

    fixed bug where computation graph was not being evaluated in the correct order.

--- autodiff.py
+++ autodiff.py
@@ -1,14 +1,15 @@
+from utils import topological_sort
+
 class ad_addition_node:
 
     def __init__(self, left, right):
         self.left = left
         self.right = right
+        self.vertices = [left, right]
 
     def backward(self, acc):
         self.left.grad += acc
         self.right.grad += acc
-        self.left.backward(self.left.grad)
-        self.right.backward(self.right.grad)
 
 
 class ad_multiplication_node:
@@ -16,12 +17,11 @@ class ad_multiplication_node:
     def __init__(self, left, right):
         self.left = left
         self.right = right
+        self.vertices = [left, right]
 
     def backward(self, acc):
         self.left.grad += self.right.value * acc
         self.right.grad += self.left.value * acc
-        self.left.backward(self.left.grad)
-        self.right.backward(self.right.grad)
 
 
 class ad_float:
@@ -30,6 +30,12 @@ class ad_float:
         self.value = value
         self.grad = 0
         self.compute_graph = compute_graph
+
+    def __str__(self):
+        return f"ad_float:\nvalue: {self.value}\ngrad: {self.grad}\ncompute_graph: {self.compute_graph}"
+
+    #def __eq__(self, other):
+    #return self.value == other.value and self.grad == other.grad and self.compute_graph == other.compute_graph
 
     def __add__(self, other):
         if not isinstance(other, ad_float):
@@ -61,10 +67,49 @@ class ad_float:
             # compute_graph=ad_substraction_node(self, other)
         )
 
-    def backward(self, acc = 1.0):
+    def __backward(self, acc = 1.0):
         print(f"value: {self.value} $$ grad: {self.grad} $$ cg: {self.compute_graph}")
         if self.compute_graph is not None:
             self.compute_graph.backward(acc)
+
+    def backward(self):
+        # Form Compute Graph
+        vertices = []
+        edges = {}
+        generate_graph(self, vertices, edges)
+        print("Printing Graph:")
+        for v in vertices:
+            print(v)
+        for v in edges.keys():
+            print(v)
+            print("Is connected to:")
+            for n in edges[v]:
+                print(n)
+
+        # Sort The Graph
+        sorted_vertices = topological_sort(vertices, edges)
+
+        # Run backward on the graph
+        sorted_vertices[0].grad = 1.0
+        print("Printing Sorted Vertices")
+        for vertex in sorted_vertices:
+            print(vertex)
+            if vertex.compute_graph is not None:
+                vertex.compute_graph.backward(vertex.grad)
+
+
+
+def generate_graph(vertex, vertices, edges):
+    if vertex not in vertices:
+        vertices.append(vertex)
+        if vertex.compute_graph is not None:
+            edges.update({vertex: vertex.compute_graph.vertices})
+        else:
+            edges.update({vertex: []})
+        for v in edges[vertex]:
+            generate_graph(v, vertices, edges)
+
+
 
 
 
